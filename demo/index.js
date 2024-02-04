a987d7bc45c595e876b0a243274d94a60415cba4
The Anh Nguyen <ntheanh201@gmail.com>
2024-02-04 17:04:52 +0100

    feat: add user's feedback component

--- demo/index.js
+++ demo/index.js
@@ -1,16 +1,51 @@
-// import React from 'react';
-import ReactDOM from 'react-dom';
-import { UserFeedback } from '../src/index';
-
-const root = document.querySelector('#root');
+import { createRoot } from 'react-dom/client';
+import UserFeedback from '../src/index';
 
 const App = () => {
+	const onSubmit = async (values, onError) => {
+		const { image, type, email, message } = values;
+
+		const formData = new FormData();
+		formData.append('images', image);
+		formData.append('type', type);
+		formData.append('path', window?.location?.pathname);
+		formData.append('message', message);
+		formData.append('email', email);
+
+		// eslint-disable-next-line no-undef
+		await axios
+			.post(`http://localhost/v1/feedback`, formData, {
+				headers: {
+					'Content-Type': 'multipart/form-data',
+				},
+			})
+			.then((res) => {
+				console.log('res: ', res.data);
+			})
+			.catch((error) => {
+				console.log('error: ', error);
+				onError(error);
+			});
+	};
+
 	return (
 		<div>
 			<h1>React User Feedback</h1>
-			<UserFeedback />
+			<h2>@ntheanh201/react-user-feedback</h2>
+			<UserFeedback
+				disabled={false}
+				allowImage={true}
+				feedbackTypes={[
+					{ value: 'general', label: 'General' },
+					{ value: 'bug', label: 'Bug' },
+					{ value: 'idea', label: 'Idea' },
+				]}
+				onSubmit={onSubmit}
+			/>
 		</div>
 	);
 };
 
-ReactDOM.render(<App />, root);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
---
+++ src/hooks/user-click-outside.ts
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+
+export const useClickOutside = (
+	ref: React.MutableRefObject<any>,
+	handler: any,
+) => {
+	useEffect(() => {
+		const listener = (event: any) => {
+			if (!ref.current || ref.current.contains(event.target)) {
+				return;
+			}
+			handler(event);
+		};
+		document.addEventListener('mousedown', listener);
+		document.addEventListener('touchstart', listener);
+		return () => {
+			document.removeEventListener('mousedown', listener);
+			document.removeEventListener('touchstart', listener);
+		};
+	}, [ref, handler]);
+};
